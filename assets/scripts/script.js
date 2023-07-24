5508dcfae829f8d40de1785d02ed0279bd93e8ba
Abuemkeze Chu <abuemkeze@gmail.com>
2023-07-24 16:59:18 +0100

    add: change background on win

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const secretNumber = Math.trunc(Math.random() * 100 + 1)
+const secretNumber = Math.trunc(Math.random() * 20 + 1)
 document.querySelector('.number').textContent = secretNumber
 let score = 20
 let highscore = 0
@@ -9,12 +9,21 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value)
   console.log(guess)
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!'
+
+    // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!'
+
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('.number').style.width = '50rem'
+
     highscore = score + highscore
     document.querySelector('.highscore').textContent = highscore
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!'
@@ -24,6 +33,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game!'
       document.querySelector('.score').textContent = 0
     }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low!'
