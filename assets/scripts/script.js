6bdf26d40e992c67c519ed6f2c6aed42e5130c68
Abuemkeze Chu <abuemkeze@gmail.com>
2023-07-25 02:40:03 +0100

    fix: lint errors

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -47,7 +47,7 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 })
 
-document.querySelector('.again').addEventListener('click', function(){
+document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20 + 1)
   document.querySelector('body').style.backgroundColor = '#222'
   document.querySelector('.number').textContent = '?'
